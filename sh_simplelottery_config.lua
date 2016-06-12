b16060b1aabad79c82595579975c8d2aa6b3a640
G0ld3neyes <G0ld3neyes@gmail.com>
2016-06-12 20:36:49 +0200

    Lottery config file
    Other files are OK.

---
+++ sh_simplelottery_config.lua
@@ -0,0 +1,18 @@
+//Don't touch this!
+NiandraLottery = NiandraLottery or {}
+
+//Mechanics
+NiandraLottery.LotteryTime = 60 -- How many seconds should a lottery last for? This is equal to 20 minutes.
+NiandraLottery.LotteryCooldown = 120 -- How many seconds, after a lottery finishes, should pass before another starts?
+
+//Rewards
+NiandraLottery.Pointshop = true -- Should players get PS points for winning?
+NiandraLottery.Pointshop_2 = false -- Should players get PS2 points for winning instead?
+NiandraLottery.DarkRPMoney = false -- Should players get DarkRP2 points for winning instead?
+
+//Lottery
+NiandraLottery.StaticNumber = 20 -- How much should players input into the lottery each time?
+
+//Access
+NiandraLottery.ChatCommand = "!lottery"
+NiandraLottery.ChatCommand_Timeleft = "!lotterytime"
